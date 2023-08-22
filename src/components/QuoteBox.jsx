5fb156a8d4ebf730ef2e7d717351c1e945f175e0
Adi <121740959+Adi020@users.noreply.github.com>
2023-08-22 02:57:08 -0600

    Actualizar QuoteBox.jsx

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,6 +1,6 @@
 const QuoteBox = ({ phrase, handleChangeQuote }) => {
   return (
-    <div className="flex flex-col items-start gap-16 pb-8">
+    <div className="flex flex-col items-center gap-16 pb-8">
       <article className="scroll-container text-4xl overflow-y-scroll max-[640px]: max-h-[400px] text-[#573675] rounded-[50px] text-center flex justify-center items-center max-w-[600px] min-h-[350px] bg-gradient-to-b from-[#b7b7d2] p-10" >
         <p className="leading-[45px] text-4xl font-bold">{phrase}</p>
       </article>
