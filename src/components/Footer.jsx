51be486a82d68563ef1d2c1460024802294c8d2b
Adi <121740959+Adi020@users.noreply.github.com>
2023-08-22 01:56:33 -0600

    Actualizar Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({author}) => {
   return (
-    <footer className="text-center absolute bottom-0 text-2xl font-bold text-[#f0f8ff94]">
+    <footer className="text-center bottom-0 text-2xl font-bold text-[#f0f8ff94]">
       <h3>Author: {author}</h3>
     </footer>
   );
