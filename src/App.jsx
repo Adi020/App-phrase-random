91ced5bc2fffb7d9e0e9ffe6a10847e59cfe5bae
Adi <121740959+Adi020@users.noreply.github.com>
2023-08-22 03:30:43 -0600

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ function App() {
 
   return (
     <main
-      className={`${bgRoute} px-7 bg-cover min-h-screen flex justify-center flex-col max-[640px]:gap-16 gap-28`}
+      className={`${bgRoute} px-7 bg-cover min-h-screen flex justify-center flex-col`}
     >
 
-<div clasName="">
+<div clasName="flex justify-center max-[640px]:gap-16 gap-28">
 <h1 className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  font-outline text-[#f0f8ffae] text-[50px]">
         INFOGALAX
       </h1>
